ab22c72368aeab7a99914d2c10796a3ccd9a433b
hwangs12 <sungjun94@gmail.com>
2024-10-12 22:25:18 -0400

    instruction

--- MACHINE.c
+++ MACHINE.c
@@ -1,1 +1,7 @@
 // a machine insruction is a unique bit pattern that a device can identify
+
+// CPU controls all activities going inside computer
+
+// in x86, 10100000 means add two numbers
+
+// 000000101 to halt a computer
